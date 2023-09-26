5560c5403abe70a15f11b3d68c408b84f58320ae
Fwhiterabbit <thisismichael91@gmail.com>
2023-09-26 20:14:04 +0000

    deployment commit

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ['DATABASE_URL'] = 'postgres://ieyvvtwb:kLe6W3z7gBEJet6f32EZ3BVkH8zrRPFH@surus.db.elephantsql.com/ieyvvtwb'
+
+os.environ['SECRET_KEY'] = '46fse$68s£56-j-wre;84k'
+
+CLOUDINARY_URL = 'cloudinary://991483988927428:dbA3Qho4wpELI3UfA-4tOHPHQf0@do9brxoes'
