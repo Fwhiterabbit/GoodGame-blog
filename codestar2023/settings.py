4459525dac94ca6a98564f377c50f5d97f1b1764
Fwhiterabbit <thisismichael91@gmail.com>
2023-09-27 20:51:21 +0000

    added templates, urls and views

--- codestar2023/settings.py
+++ codestar2023/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-fwhiterabbi-goodgameblo-15ud1paok4t.ws-eu104.gitpod.io', 'codestar2023']
+ALLOWED_HOSTS = ['8000-fwhiterabbi-goodgameblo-15ud1paok4t.ws-eu105.gitpod.io', 'codestar2023']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'gg_blog',
 ]
 
