18f71d3806561264d12cb4ec8a930cf9f80a53d2
Fwhiterabbit <thisismichael91@gmail.com>
2023-10-02 18:10:08 +0000

    allowed host issue

--- codestar2023/settings.py
+++ codestar2023/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['gg-blog-27459f85e928.herokuapp.com', 'codestar2023'],
+ALLOWED_HOSTS = ['gg-blog-27459f85e928.herokuapp.com', 'codestar2023']
 
 
 
